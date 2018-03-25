585ff193786bc824a215a850955c6e61ff3b784b
Carlos <sanchez.carlos.1986@gmail.com>
2018-03-25 09:10:15 -0300

    feat: new components to show weather

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import React, { Component } from 'react';
-import Button from './UI/Button'
+import React, { Component } from 'react'
+import WeatherLocation from './components/WeatherLocation'
 
 class App extends Component {
-  constructor(){
-    super()
-
-    this.state = { title: 'Clickea un botón' }
-  }
-
-  onLogin = e => {
-    console.log(`Click: ${e.target.innerText}`)
-
-    this.setState({
-      title: e.target.innerText
-    })
-  }
-  
   render() {
-    const { title } = this.state
     return (
       <div className="App">
-        <h1>{title}</h1>
-        <Button onClick={this.onLogin} text='Button'></Button>
-        <Button primary onClick={this.onLogin} text='Button Primary'></Button>
-        <Button success onClick={this.onLogin} text='Button Success'></Button>
-        <Button warning onClick={this.onLogin} text='Button Warning'></Button>
-        <Button danger onClick={this.onLogin} text='Button Danger'></Button>
+        <WeatherLocation />
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/components/WeatherData.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import WeatherTemperature from './WeatherTemperature'
+import WeatherExtraInfo from './WeatherExtraInfo'
+
+const WeatherData = () => (
+   <div>
+      <WeatherTemperature />
+      <WeatherExtraInfo humidity={80} wind={'10m/s'} />
+   </div>
+)
+
+export default WeatherData
