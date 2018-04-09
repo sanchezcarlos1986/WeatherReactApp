a3128085b43e79618a2da03db93cadc71ef62481
Carlos Sanchez <csanchez@eclass.cl>
2018-04-09 10:39:47 -0300

    Provider, store and mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import LocationList from './components/LocationList'
 import ForecastExtended from './components/ForecastExtended'
+import { setCity } from './actions'
 
 class App extends Component {
   constructor() {
     super()
-    this.state = { city: null }
+		this.state = { city: null }
   }
 
   handleSelectedLocation = city => {
-    this.setState({city})
+		this.props.setCity(city)
+		this.setState({city})
   }
-  
+
   render() {
+		console.log('props de App', this.props)
     const { city } = this.state
     return (
       <div className="App">
@@ -26,4 +31,16 @@ class App extends Component {
   }
 }
 
-export default App
+App.propTypes = {
+  setCity: PropTypes.func.isRequired
+}
+
+const mapStateToProps = null
+
+// mapDispatchToProps retorna un objeto que despacha una acción, en este caso se despacha la acción "SET_CITY" a través del action creator "setCity"
+const mapDispatchToProps = dispatch => ({
+	setCity: value => dispatch(setCity(value))
+})
+
+// Conectamos App a la store para que pueda recibir el estado y las acciones como props y lo exportamos
+export default connect(mapStateToProps, mapDispatchToProps)(App)
